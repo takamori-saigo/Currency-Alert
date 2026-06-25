de7eb03a6b1571686f0ead12cebc4207dea5eeec
takamori-saigo <98211881+takamori-saigo@users.noreply.github.com>
2026-06-25 08:51:12 +0500

    base configure pipline

--- Currency-Alert/Program.cs
+++ Currency-Alert/Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+if (app.Environment.IsProduction())
+    app.UseHsts();
+app.UseHttpsRedirection();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions{ForwardedHeaders = ForwardedHeaders.All} );
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
